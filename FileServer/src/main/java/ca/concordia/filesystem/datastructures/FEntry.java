7dc2779c3046d36a3d5e2e541bd2788c4c590e55
Khogulan Thambu <khogulanthambu@gmail.com>
2025-11-11 21:39:30 -0500

    Changes to FEntry

--- FileServer/src/main/java/ca/concordia/filesystem/datastructures/FEntry.java
+++ FileServer/src/main/java/ca/concordia/filesystem/datastructures/FEntry.java
@@ -1,14 +1,17 @@
 package ca.concordia.filesystem.datastructures;
 
 import java.util.LinkedList;
+import java.io.Serializable;
 
-public class FEntry {
+public class FEntry implements Serializable {
+    private static final long serialVersionUID = 1L;    //Serialization so if changes occur in code, it won't break deserialization.
 
-    private String filename;
-    private short filesize;
-    private short firstBlock; // Pointers to data blocks
+    private String filename;    // Max 11 characters
+    private short filesize;     // Size in bytes
+    private short firstBlock;   // Pointer to the first data block
 
     public FEntry(String filename, short filesize, short firstblock) throws IllegalArgumentException{
+        
         //Check filename is max 11 bytes long
         if (filename.length() > 11) {
             throw new IllegalArgumentException("Filename cannot be longer than 11 characters.");
@@ -43,5 +46,22 @@ public class FEntry {
 
     public short getFirstBlock() {
         return firstBlock;
+    }
+
+    //Check if entry is used
+    public boolean isUsed() {
+        return filename != null && !filename.isEmpty();
+    }
+
+    //Clear entry data(delete option)
+    public void clear() {
+        this.filename = null;
+        this.filesize = 0;
+        this.firstBlock = -1;
+    }
+
+    @Override
+    public String toString() {
+        return "FEntry[filename= " + filename + ", filesize= " + filesize + ", firstBlock= " + firstBlock + "]";
     }
 }
